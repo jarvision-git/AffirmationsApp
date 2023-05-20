ba8a2f6f130bcea9b16fba06f33ba4691b8c4611
Kushal <kushalldesai@gmail.com>
2023-05-20 10:29:52 +0530

    Final app without challenge

---
+++ app/src/main/java/com/example/affitmations/model/Affirmation.kt
@@ -0,0 +1,13 @@
+package com.example.affitmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int)
+{
+
+}
+
+
